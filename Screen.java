88e67b915b3b3c76cfcb9a2c069dd1d57f6d351f
Kalanidis <dimitriskaln@gmail.com>
2022-04-09 18:38:00 +0200

    secondary panel
    added secondary panel in the center, same color as main panel

--- Screen.java
+++ Screen.java
@@ -1,21 +1,21 @@
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.plaf.FontUIResource;
 
 
 public class Screen{
 
     JFrame mainWindow;
     static Dimension d=Toolkit.getDefaultToolkit().getScreenSize();
-    private static final double WIDTH = d.getWidth();
-    private static final double HEIGHT = d.getHeight();
+
     JLabel mainTitle;
     JPanel mainPanel;
+    JPanel inputPanel;
 
 
 
@@ -30,13 +30,31 @@ public class Screen{
 
    public void toggleMainPanel(){
         
+        //Main panel
+        
         mainPanel=new JPanel();
         mainPanel.setLayout(null);
+        mainPanel.setBackground(new Color(57,17,165));
+        
+        //Title on the main panel
+
         mainTitle=new JLabel("Welcome. Please insert your account name and your password");
         mainTitle.setBounds(700, 50, 600,50);
         mainTitle.setFont(new Font("Serif",Font.BOLD,16));
+        mainTitle.setForeground(Color.white);
+        
+        //Secondary Panle inside the main Panel
+
+        inputPanel=new JPanel();
+        inputPanel.setBackground(new Color(57,17,165));
+        inputPanel.setBounds(700, 300, 600, 400);
+
+        //Adding everything together
+
+        mainPanel.add(inputPanel);
         mainPanel.add(mainTitle);
         mainWindow.add(mainPanel);
+        
 
     }
 }
