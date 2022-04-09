f6d2c1dd912227d6f6c04633f3d8e7a9b67ccf13
Kalanidis <dimitriskaln@gmail.com>
2022-04-09 18:00:44 +0200

    Good Start
    added title and panel

--- Screen.java
+++ Screen.java
@@ -1,12 +1,22 @@
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Toolkit;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.plaf.FontUIResource;
+
 
 public class Screen{
 
     JFrame mainWindow;
     static Dimension d=Toolkit.getDefaultToolkit().getScreenSize();
+    private static final double WIDTH = d.getWidth();
+    private static final double HEIGHT = d.getHeight();
+    JLabel mainTitle;
+    JPanel mainPanel;
+
 
 
     public Screen(){
@@ -15,6 +25,18 @@ public class Screen{
         mainWindow.setVisible(true);
         mainWindow.setSize(d);
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        mainPanel=new JPanel();
+        mainPanel.setLayout(null);
+
+        mainTitle=new JLabel("Welcome. Please insert your account name and your password");
+        mainTitle.setBounds(700, 50, 600,50);
+        mainTitle.setFont(new Font("Serif",Font.BOLD,16));
+
+        mainPanel.add(mainTitle);
+        mainWindow.add(mainPanel);
+        
+    
     }
 
    
